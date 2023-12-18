0d7156677d02edef8e5fd255390a852bd74b2cb6
Davi Costa <davicostadelira04@gmail.com>
2023-12-18 19:16:13 -0300

    Order, Instant and ISO 8601

---
+++ src/main/java/com/devdavicl/webservice/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.devdavicl.webservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devdavicl.webservice.entities.Order;
+import com.devdavicl.webservice.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
