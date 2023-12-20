25e2d51f156bec98efb2a3de43b8b226c210319c
Davi Costa <davicostadelira04@gmail.com>
2023-12-20 16:43:49 -0300

    Exception handling - findById

--- src/main/java/com/devdavicl/webservice/services/UserService.java
+++ src/main/java/com/devdavicl/webservice/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.devdavicl.webservice.entities.User;
 import com.devdavicl.webservice.repositories.UserRepository;
+import com.devdavicl.webservice.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
